a36c68dbd6230d80b01d0c2984d126aa97a30a65
Richard Peralta <peraltar91@my.sunyulster.edu>
2020-05-10 20:19:13 -0400

    Initial Commit

---
+++ Sleep Tracker/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Sleep Tracker
+//
+//  Created by Richard Peralta on 5/10/20.
+//  Copyright © 2020 Richard Peralta. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Sleep Tracker/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Sleep Tracker
+//
+//  Created by Richard Peralta on 5/10/20.
+//  Copyright © 2020 Richard Peralta. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
