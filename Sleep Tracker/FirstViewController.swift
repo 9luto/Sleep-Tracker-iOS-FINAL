8c44e7a9fd4beeaf345c188c77a76a903e0ad8e1
Richard Peralta <peraltar91@my.sunyulster.edu>
2020-05-11 10:36:21 -0400

    first commit. testing for first tab needed.

--- Sleep Tracker/FirstViewController.swift
+++ Sleep Tracker/FirstViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    let formatter = DateFormatter()
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        formatter.dateStyle = .none
+        formatter.timeStyle = .short
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateClock), userInfo: nil, repeats: true)
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBOutlet weak var clock: UILabel!
+    @IBOutlet weak var userWakeUp: UIDatePicker!
+    @IBAction func wakeUpChosen(_ sender: UIDatePicker) {
 
-
+        let wakeUpTime = userWakeUp.date
+        let sleepTime = formatter.string(from: wakeUpTime.addingTimeInterval(5 * 90))
+        let sleepTimeAlt = formatter.string(from: wakeUpTime.addingTimeInterval(6 * 90))
+        
+        let sleepString = "you should to go sleep at: " + sleepTime + "or" + sleepTimeAlt + "to feel refreshed in the morning"
+        
+        sleepPrint.text = sleepString
+    }
+    @IBOutlet weak var sleepPrint: UILabel!
+    
+     @objc public func updateClock()
+    {
+        let currentDateTime = Date()
+        clock.text = formatter.string(from: currentDateTime)
+    }
 }
 
